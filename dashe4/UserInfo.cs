df7ae9f7923c4831826cb935395bacb807c18ac9
kraxarn <emil202821@hotmail.com>
2018-06-26 18:20:59 +0200

    Add some info

--- dashe4/UserInfo.cs
+++ dashe4/UserInfo.cs
@@ -12,9 +12,24 @@ namespace dashe4
 		public EClanPermission Rank;
 		public EChatPermission Permission;
 
+		/// <summary>
+		/// Last time user got a warning?
+		/// </summary>
 		public DateTime LastTime;
+
+		/// <summary>
+		/// Last time the user sent a message in the chat
+		/// </summary>
 		public DateTime LastMessage;
+
+		/// <summary>
+		/// Last the they joined the chat
+		/// </summary>
 		public DateTime LastJoin;
+
+		/// <summary>
+		/// Last time they left the chat
+		/// </summary>
 		public DateTime LastLeave;
 
 		public int Disconnects;
